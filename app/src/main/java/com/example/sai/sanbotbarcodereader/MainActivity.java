fc5e7cd656cffbe8fa052349c72a2f9c0dd1f1f9
krrishbot <sai@exaptec.com.au>
2017-10-08 13:17:01 +1100

    successfully  reads barcodes

---
+++ app/src/main/java/com/example/sai/sanbotbarcodereader/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.sai.sanbotbarcodereader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import me.dm7.barcodescanner.zbar.Result;
+import me.dm7.barcodescanner.zbar.ZBarScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZBarScannerView.ResultHandler {
+    private ZBarScannerView mScannerView;
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        mScannerView = new ZBarScannerView(this);    // Programmatically initialize the scanner view
+        setContentView(mScannerView);                // Set the scanner view as the content view
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();          // Start camera on resume
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+        Log.d(TAG, rawResult.getContents()); // Prints scan results
+        Log.d(TAG, rawResult.getBarcodeFormat().getName()); // Prints the scan format (qrcode, pdf417 etc.)
+
+        // If you would like to resume scanning, call this method below:
+        mScannerView.resumeCameraPreview(this);
+    }
+}
